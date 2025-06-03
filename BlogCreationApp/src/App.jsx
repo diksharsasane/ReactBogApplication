6ac2884c0a16983d79ab9f8e4219cd35986d5922
diksharsasane <dikshasasane23@gmail.com>
2025-06-04 00:39:56 +0530

    configured redux-toolkit

--- BlogCreationApp/src/App.jsx
+++ BlogCreationApp/src/App.jsx
@@ -1,14 +1,37 @@
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import {login,logout} from './store/authSlice'
+import {Header,Footer} from './components/index.jsx'
 
 function App() {
+  const [loading,setLoading]=useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  },[])
   
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  
-  return (
-    <>
-      <h1> checking Reacttttt blog</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-grey-400'>
+      <div className="w-full block">
+        <Header/>
+        <main>
+          Todo: {/*<Outlet/>*/}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
